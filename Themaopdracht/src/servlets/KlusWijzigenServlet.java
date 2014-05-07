8db1404625efe4864554ff7f360b2862f3f48a48
Jfeurich <jayh4x0r@gmail.com>
2014-05-07 16:35:40 +0200

    nog een keer

--- Themaopdracht/src/servlets/KlusWijzigenServlet.java
+++ Themaopdracht/src/servlets/KlusWijzigenServlet.java
@@ -1,5 +1,5 @@
 package servlets;
-//Resultaten uit het verleden bieden geen garanties voor de toekomst
+//Resultaten uit het verleden bieden geen garanties voor de toekomst blijkt maar weer
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
