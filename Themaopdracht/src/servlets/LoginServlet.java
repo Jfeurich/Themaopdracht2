d0f8e9f37e06c2fd4d1d7af49e4717105534d706
p4k03n4t0r <paul__94@hotmail.com>
2014-05-19 10:16:27 +0200

    Loginservlet faalde met pushen dus poging 2

--- Themaopdracht/src/servlets/LoginServlet.java
+++ Themaopdracht/src/servlets/LoginServlet.java
@@ -1,68 +1,50 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import database.ConnectDB;
+import database.ConnectDBUser;
 import domeinklassen.User;
 
-public class LogInServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private ArrayList<User> deGebruikers;
 	private User deGebruiker;
 	
-	public void init(ServletConfig config) throws ServletException{
-		super.init(config);
-		String url = config.getInitParameter("connectDB");
-		System.out.println(url);
-	}
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException { 
+		
+		ConnectDB database = new ConnectDB();
+		Connection con = database.maakVerbinding();
+		
 		boolean loginSuccess = false; 
 		String username = req.getParameter("username"); 
 		String password = req.getParameter("password"); 
 		if(username != null && password != null){
 			//login check
-			deGebruikers = (ArrayList<User>) getServletContext().getAttribute("users");
-			if(deGebruikers == null){
-				deGebruikers = new ArrayList<User>();
-			}
+			ConnectDBUser usercon = new ConnectDBUser(con);
 			boolean b = false;
-			for(User u: deGebruikers){
+			for(User u: usercon.getUsers()){
 				if(u.getGebruikersnaam().equals(username) && u.getWachtwoord().equals(password)){
 					deGebruiker = u;
 					b = true;
 				}
 			}			
 			if (!b) { 
-				req.setAttribute("msgs", "Username and/or password are incorrect!"); 
+				req.setAttribute("msgs", "Onjuist wachtwoord!"); 
 			} 
 			else{
 				ArrayList<String> ingelogdeGebruikers = (ArrayList<String>) getServletContext().getAttribute("loggedusers");
-				b = true;
-				if(ingelogdeGebruikers == null){
-					ingelogdeGebruikers = new ArrayList<String>();
-				}
-				for(String s: ingelogdeGebruikers){
-					if(s.equals(deGebruiker.getGebruikersnaam())){
-						b = false;
-					}
-				}
-				if(b){
-					ingelogdeGebruikers.remove("username");
-					getServletContext().setAttribute("loggedusers", ingelogdeGebruikers);
-					req.setAttribute("msgs", "U bent succesvol ingelogd!");
-					loginSuccess = true;
-				}
-				else{
-					req.setAttribute("msgs", "U bent reeds ingelogd!");
-				}
+				getServletContext().setAttribute("loggedusers", ingelogdeGebruikers);
+				req.setAttribute("msgs", "U bent succesvol ingelogd!");
+				loginSuccess = true;
 			}
 		}
 		else{
@@ -76,9 +58,10 @@ public class LogInServlet extends HttpServlet {
 			req.getSession().setAttribute("gebruiker", deGebruiker);
 		}
 		else{
-			rd = req.getRequestDispatcher("login.jsp");
+			rd = req.getRequestDispatcher("loginpage.jsp");
 		}
 		rd.forward(req, resp); 
+		database.sluitVerbinding(con);
 	}
 } 
 
