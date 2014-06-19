5d7df4ac4b77feee2b19201c8ca730a5827272f3
jiry1994 <jiry1994@live.nl>
2014-06-19 14:52:54 +0200

    teeeeeeeeeeeeests

---
+++ Themaopdracht/src/com/example/tests/US26_Klus_Annuleren.java
@@ -0,0 +1,69 @@
+package com.example.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class US26_Klus_Annuleren {
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost:8080/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testKlusAnnuleren() throws Exception {
+		driver.get(baseUrl + "/Themaopdracht/loginpage.jsp");
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("username")).sendKeys("Henk");
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("hww");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		driver.get("http://localhost:8080/Themaopdracht/klus.jsp");
+		driver.findElement(By.name("zoekid")).clear();
+		driver.findElement(By.name("zoekid")).sendKeys("1");
+		driver.findElement(By.xpath("(//input[@name='knop'])[3]")).click();
+		driver.findElement(By.name("gekozenklus")).click();
+		driver.findElement(By.xpath("(//input[@name='knop'])[5]")).click();
+		driver.findElement(By.xpath("(//input[@name='knop'])[3]")).click();
+		assertEquals("Geen klussen gevonden met ingevoerde zoekterm(en)",
+				driver.findElement(By.cssSelector("h3.msg > span")).getText());
+		driver.findElement(By.name("zoekid")).clear();
+		driver.findElement(By.name("zoekid")).sendKeys("abc");
+		driver.findElement(By.xpath("(//input[@name='knop'])[3]")).click();
+		assertTrue(isElementPresent(By.cssSelector("h3.error")));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+}
---
+++ Themaopdracht/src/com/example/tests/US9_Status_Van_Klussen_Bekijken.java
@@ -0,0 +1,45 @@
+package com.example.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class US9_Status_Van_Klussen_Bekijken {
+  private WebDriver driver;
+  private String baseUrl;
+  @SuppressWarnings("unused")
+private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testStatusVanKlussen() throws Exception {
+    driver.get(baseUrl + "/Themaopdracht/loginpage.jsp");
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("Sandri");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("sww");
+    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+    assertEquals("U heeft geen aankomende klussen", driver.findElement(By.xpath("//div[@id='content']/div[3]")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
